b9ea6a0353a62f3a6473778ccf8ca13c22d857dc
Vesa Meriläinen <vesamatti.merilainen93@gmail.com>
2019-07-09 14:22:29 +0300

    add help texts

---
+++ src/components/Question.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="question" v-show="questionnum == 0">
+    <div class="question_text">
+      <p>1. {{ $t('message.question_health') }}</p>
+      <button class="btn help_button" @click.prevent="toggleQuestionHelp('health')">?</button>
+    </div>
+    <p v-if="help_text_visible === 'health'">{{ $t('message.help_text_health') }}</p>
+    <b-form-input type="range" min="0" max="10" v-model="questiondata.health" />
+    <p>{{questiondata.health}}</p>
+    <textarea v-model="questiondata.health_desc" rows="3" v-bind:placeholder="$t('message.question_desc_placeholder')"></textarea>
+    <div class="buttons">
+      <button class="btn btn-primary" @click.prevent="toNextQuestion">{{ $t('message.next') }}</button>
+    </div>
+    <router-link to="/">{{ $t('message.cancel')}}</router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: 'question'
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
--- src/views/Questionnaire.vue
+++ src/views/Questionnaire.vue
@@ -10,7 +10,11 @@
     <div class="questionnaire container col-md-8 text-center">
       <form v-on:submit.prevent="saveQuestions">
         <div class="question" v-show="questionnum == 0">
-          <p>1. {{ $t('message.question_health') }}</p>
+          <div class="question_text">
+            <p>1. {{ $t('message.question_health') }}</p>
+            <button class="btn help_button" @click.prevent="toggleQuestionHelp('health')">?</button>
+          </div>
+          <p v-if="help_text_visible === 'health'">{{ $t('message.help_text_health') }}</p>
           <b-form-input type="range" min="0" max="10" v-model="questiondata.health" />
           <p>{{questiondata.health}}</p>
           <textarea v-model="questiondata.health_desc" rows="3" v-bind:placeholder="$t('message.question_desc_placeholder')"></textarea>
@@ -20,7 +24,11 @@
           <router-link to="/">{{ $t('message.cancel')}}</router-link>
         </div>
         <div class="question" v-show="questionnum == 1">
-          <p>2. {{ $t('message.question_overcoming') }}</p>
+          <div class="question_text">
+            <p>2. {{ $t('message.question_overcoming') }}</p>
+            <button class="btn help_button" @click.prevent="toggleQuestionHelp('overcoming')">?</button>
+          </div>
+          <p v-if="help_text_visible === 'overcoming'">{{ $t('message.help_text_overcoming') }}</p>
           <b-form-input type="range" min="0" max="10" v-model="questiondata.overcoming" />
           <p>{{questiondata.overcoming}}</p>
           <textarea v-model="questiondata.overcoming_desc" rows="3" v-bind:placeholder="$t('message.question_desc_placeholder')"></textarea>
@@ -31,7 +39,11 @@
           <router-link to="/">{{ $t('message.cancel')}}</router-link>
         </div>
         <div class="question" v-show="questionnum == 2">
-          <p>3. {{ $t('message.question_living') }}</p>
+          <div class="question_text">
+            <p>3. {{ $t('message.question_living') }}</p>
+            <button class="btn help_button" @click.prevent="toggleQuestionHelp('living')">?</button>
+          </div>
+          <p v-if="help_text_visible === 'living'">{{ $t('message.help_text_living') }}</p>
           <b-form-input type="range" min="0" max="10" v-model="questiondata.living" />
           <p>{{questiondata.living}}</p>
           <textarea v-model="questiondata.living_desc" rows="3" v-bind:placeholder="$t('message.question_desc_placeholder')"></textarea>
@@ -42,7 +54,11 @@
           <router-link to="/">{{ $t('message.cancel')}}</router-link>
         </div>
         <div class="question" v-show="questionnum == 3">
-          <p>4. {{ $t('message.question_coping') }}</p>
+          <div class="question_text">
+            <p>4. {{ $t('message.question_coping') }}</p>
+            <button class="btn help_button" @click.prevent="toggleQuestionHelp('coping')">?</button>
+          </div>
+          <p v-if="help_text_visible === 'coping'">{{ $t('message.help_text_coping') }}</p>
           <b-form-input type="range" min="0" max="10" v-model="questiondata.coping" />
           <p>{{questiondata.coping}}</p>
           <textarea v-model="questiondata.coping_desc" rows="3" v-bind:placeholder="$t('message.question_desc_placeholder')"></textarea>
@@ -53,7 +69,11 @@
           <router-link to="/">{{ $t('message.cancel')}}</router-link>
         </div>
         <div class="question" v-show="questionnum == 4">
-          <p>5. {{ $t('message.question_family') }}</p>
+          <div class="question_text">
+            <p>5. {{ $t('message.question_family') }}</p>
+            <button class="btn help_button" @click.prevent="toggleQuestionHelp('family')">?</button>
+          </div>
+          <p v-if="help_text_visible === 'family'">{{ $t('message.help_text_family') }}</p>
           <b-form-input type="range" min="0" max="10" v-model="questiondata.family" />
           <p>{{questiondata.family}}</p>
           <textarea v-model="questiondata.family_desc" rows="3" v-bind:placeholder="$t('message.question_desc_placeholder')"></textarea>
@@ -64,7 +84,11 @@
           <router-link to="/">{{ $t('message.cancel')}}</router-link>
         </div>
         <div class="question" v-show="questionnum == 5">
-          <p>6. {{ $t('message.question_friends') }}</p>
+          <div class="question_text">
+            <p>6. {{ $t('message.question_friends') }}</p>
+            <button class="btn help_button" @click.prevent="toggleQuestionHelp('friends')">?</button>
+          </div>
+          <p v-if="help_text_visible === 'friends'">{{ $t('message.help_text_friends') }}</p>
           <b-form-input type="range" min="0" max="10" v-model="questiondata.friends" />
           <p>{{questiondata.friends}}</p>
           <textarea v-model="questiondata.friends_desc" rows="3" v-bind:placeholder="$t('message.question_desc_placeholder')"></textarea>
@@ -75,7 +99,11 @@
           <router-link to="/">{{ $t('message.cancel')}}</router-link>
         </div>
         <div class="question" v-show="questionnum == 6">
-          <p>{{ $t('message.question_finance') }}</p>
+          <div class="question_text">
+            <p>7. {{ $t('message.question_finance') }}</p>
+            <button class="btn help_button" @click.prevent="toggleQuestionHelp('finance')">?</button>
+          </div>
+          <p v-if="help_text_visible === 'finance'">{{ $t('message.help_text_finance') }}</p>
           <b-form-input type="range" min="0" max="10" v-model="questiondata.finance" />
           <p>{{questiondata.finance}}</p>
           <textarea v-model="questiondata.finance_desc" rows="3" v-bind:placeholder="$t('message.question_desc_placeholder')"></textarea>
@@ -86,7 +114,11 @@
           <router-link to="/">{{ $t('message.cancel')}}</router-link>
         </div>
         <div class="question" v-show="questionnum == 7">
-          <p>{{ $t('message.question_strengths') }}</p>
+          <div class="question_text">
+            <p>8. {{ $t('message.question_strengths') }}</p>
+            <button class="btn help_button" @click.prevent="toggleQuestionHelp('strengths')">?</button>
+          </div>
+          <p v-if="help_text_visible === 'strengths'">{{ $t('message.help_text_strengths') }}</p>
           <b-form-input type="range" min="0" max="10" v-model="questiondata.strengths" />
           <p>{{questiondata.strengths}}</p>
           <textarea v-model="questiondata.strengths_desc" rows="3" v-bind:placeholder="$t('message.question_desc_placeholder')"></textarea>
@@ -97,7 +129,11 @@
           <router-link to="/">{{ $t('message.cancel')}}</router-link>
         </div>
         <div class="question" v-show="questionnum == 8">
-          <p>{{ $t('message.question_self_esteem') }}</p>
+          <div class="question_text">
+            <p>9. {{ $t('message.question_self_esteem') }}</p>
+            <button class="btn help_button" @click.prevent="toggleQuestionHelp('self_esteem')">?</button>
+          </div>
+          <p v-if="help_text_visible === 'self_esteem'">{{ $t('message.help_text_self_esteem') }}</p>
           <b-form-input type="range" min="0" max="10" v-model="questiondata.self_esteem" />
           <p>{{questiondata.self_esteem}}</p>
           <textarea v-model="questiondata.self_esteem_desc" rows="3" v-bind:placeholder="$t('message.question_desc_placeholder')"></textarea>
@@ -108,7 +144,11 @@
           <router-link to="/">{{ $t('message.cancel')}}</router-link>
         </div>
         <div class="question" v-show="questionnum == 9">
-          <p>{{ $t('message.question_life_as_whole') }}</p>
+          <div class="question_text">
+            <p>10. {{ $t('message.question_life_as_whole') }}</p>
+            <button class="btn help_button" @click.prevent="toggleQuestionHelp('life_as_whole')">?</button>
+          </div>
+          <p v-if="help_text_visible === 'life_as_whole'">{{ $t('message.help_text_life_as_whole') }}</p>
           <b-form-input type="range" min="0" max="10" v-model="questiondata.life_as_whole" />
           <p>{{questiondata.life_as_whole}}</p>
           <textarea v-model="questiondata.life_as_whole_desc" rows="3" v-bind:placeholder="$t('message.question_desc_placeholder')"></textarea>
@@ -202,7 +242,8 @@ export default {
         life_as_whole_desc: null
       },
       questionnum: 0,
-      help_visible: false
+      help_visible: false,
+      help_text_visible: ''
     }
   },
   methods: {
@@ -228,6 +269,13 @@ export default {
     },
     toggleHelp() {
       this.help_visible = !this.help_visible
+    },
+    toggleQuestionHelp(question) {
+      if (this.help_text_visible === question) {
+        this.help_text_visible = ''
+      } else {
+        this.help_text_visible = question
+      }
     }
   }
 }
@@ -260,6 +308,15 @@ export default {
   flex-flow: column nowrap;
   justify-content: space-between;
   height: 75vh;
+  .question_text {
+    display: flex;
+    flex-flow: row nowrap;
+    .help_button {
+      background-color: lightblue;
+      width: 2rem;
+      height: 2rem;
+    }
+  }
 }
 
 .buttons {
