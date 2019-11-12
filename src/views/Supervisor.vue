e393f01e67b6d47ae2c594a158b1ca638cf09fbd
Zohaib Malik <zohaibmalik100.zm@gmail.com>
2019-11-12 10:06:56 +0200

    update supervisor

--- src/views/Supervisor.vue
+++ src/views/Supervisor.vue
@@ -1,18 +1,19 @@
 <template>
-<div>
-  <div v-if="!loggedIn">
+<div class="supervisorPage">
+  <p class="welcome">Tervetuloa</p>
+  <div v-if="!loggedIn" class="supervisorContent">
     <label>Tunnus: </label><input v-model="supervisor_username" type="text" />
-    <label>Salasana: </label><input v-model="supervisor_password" type="text" />
-    <button @click.prevent="handleLogin">Kirjaudu sisään</button>
+    <label class="passwordLabel">Salasana: </label><input v-model="supervisor_password" type="password" />
+    <button @click.prevent="handleLogin" class="loginButton">Kirjaudu sisään</button>
   </div>
-  <div v-if="loggedIn">
-    <ul>
-      <li v-for="admin in admins" :key="admin.userId">{{admin.email}} <button @click.prevent="handleAdminRemove(admin.userId)">poista</button></li>
-    </ul>
-    <h3>Lisää uusi hallinnoija</h3>
+  <div v-if="loggedIn" class="createNew">
+    <h3 class="heading">Lisää uusi hallinnoija</h3>
     <label>Sähköposti: </label><input v-model="username" type="text" />
-    <label>Salasana: </label><input v-model="password" type="text" />
-    <button @click.prevent="handleAdminAdd">Lisää</button>
+    <label class="emailPassword">Salasana: </label><input v-model="password" type="password" />
+    <button @click.prevent="handleAdminAdd" class="createButton">Lisää</button>
+    <ul>
+      <li v-for="admin in admins" :key="admin.userId">{{admin.email}} <button @click.prevent="handleAdminRemove(admin.userId)">Poista</button></li>
+    </ul>
   </div>
 </div>
 </template>
@@ -87,5 +88,61 @@ export default {
 <style lang="scss" scoped>
 li {
   list-style: none;
+}
+.supervisorPage{
+  background-color:#FFFFFF;
+  width:100%;
+  height: 100%;
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  font-family: Arial, Helvetica, sans-serif;
+  font-style:normal;
+
+  .welcome{
+    font-size:1.1rem;
+    font-weight:bold;
+    color: #350E7E;
+    margin-top:5rem;
+  }
+
+  .supervisorContent{
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    margin-top:1rem;
+
+    .passwordLabel{
+      margin-top:1rem;
+    }
+    .loginButton{
+      background-color: #350E7E;
+      color:#FFFFFF;
+      margin-top:1rem;
+    }
+  }
+  .createNew{
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    margin-top:1rem;
+
+    .heading{
+      font-size:1.1rem;
+      font-weight:bold;
+      color: #350E7E;
+      margin-bottom:1rem;
+    }
+    .emailPassword{
+      margin-top:1rem;
+    }
+    .createButton{
+      background-color: #350E7E;
+      color:#FFFFFF;
+      margin-top:1rem;
+      margin-bottom:1.8rem;
+      padding:0 1rem;
+    }
+  }
 }
 </style>
