0bac959c88b04af7a0b18ce2c6baed13c32ef8a6
Topias Rytkönen <topias.rytkonen@digitalentshelsinki.fi>
2019-12-16 13:49:20 +0200

    Update supervisor endpoint urls

--- src/views/Supervisor.vue
+++ src/views/Supervisor.vue
@@ -1,12 +1,11 @@
 <template>
 <div class="supervisorPage">
   <p class="welcome">Tervetuloa</p>
-  <div v-if="!loggedIn" class="supervisorContent">
-    <label>Tunnus: </label><input v-model="supervisor_username" type="text" />
+  <div v-if="!jwt" class="supervisorContent">
     <label class="passwordLabel">Salasana: </label><input v-model="supervisor_password" type="password" />
     <button @click.prevent="handleLogin" class="loginButton">Kirjaudu sisään</button>
   </div>
-  <div v-if="loggedIn" class="createNew">
+  <div v-if="jwt" class="createNew">
     <h3 class="heading">Lisää uusi hallinnoija</h3>
     <label>Sähköposti: </label><input v-model="username" type="text" />
     <label class="emailPassword">Salasana: </label><input v-model="password" type="password" />
@@ -24,12 +23,11 @@ export default {
   name: 'supervisor',
   data() {
     return {
-      loggedIn: false,
-      supervisor_username: null,
       supervisor_password: null,
+      jwt: null,
       username: null,
       password: null,
-      admins: []
+      admins: [],
     }
   },
   methods: {
@@ -37,51 +35,56 @@ export default {
       axios({
         method: "POST",
         url: process.env.VUE_APP_BACKEND + "/supervisor/admin/create",
+        headers: {
+          'Authorization': `Bearer ${this.jwt}`
+        },
         data: {
           username: this.$data.username,
           password: this.$data.password
         }
-      }).then(result => {
-        this.getAdmins()
+      }).then(res => {
+        if (res.status === 200) this.getAdmins()
       })
     },
     handleAdminRemove(id) {
       axios({
         method: "POST",
         url: process.env.VUE_APP_BACKEND + "/supervisor/admin/delete",
+        headers: {
+          'Authorization': `Bearer ${this.jwt}`
+        },
         data: {
           id: id
         }
-      }).then(result => {
-        this.getAdmins()
+      }).then(res => {
+        if (res.status === 200) this.getAdmins()
       })
     },
     getAdmins() {
       axios({
         method: 'GET',
-        url: `${process.env.VUE_APP_BACKEND}/supervisor/admin/all`
-      }).then(result => {
-        this.admins = result.data
+        url: `${process.env.VUE_APP_BACKEND}/supervisor/admin/all`,
+        headers: {
+          'Authorization': `Bearer ${this.jwt}`
+        },
+      }).then(res => {
+        if (res.status === 200) this.admins = res.data
       })
     },
     handleLogin() {
       axios({
         method: 'POST',
-        url: `${process.env.VUE_APP_BACKEND}/supervisor/authorize`,
+        url: `${process.env.VUE_APP_BACKEND}/supervisor/login`,
         data: {
-          username: this.$data.supervisor_username,
           password: this.$data.supervisor_password
         }
-      }).then(result => {
-        if (result.data.success === true) {
-          this.loggedIn = true
-        } else {
+      }).then(res => {
+        if (res.status === 200) {
+          this.jwt = res.data.token
+          this.getAdmins()
         }
       })
     }
-  },
-  created() {
-    this.getAdmins()
   }
 }
 </script>
